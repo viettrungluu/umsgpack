e081d265be205de0c33a16e6e6460d053f322caa
Viet-Trung Luu <viettrungluu@gmail.com>
2024-07-09 22:08:35 -0700

    Add MarshalMapTransformer.

--- transformers.go
+++ transformers.go
@@ -1,13 +1,15 @@
 // Copyright 2024 Viet-Trung Luu.
 // Use of this source code is governed by the license in the LICENSE file.
 
-// This file contains MarshalArrayTransformer.
+// This file contains standard transformers (for use with Marshal).
 
 package umsgpack
 
 import (
 	"reflect"
 )
+
+// MarshalArrayTransformer -------------------------------------------------------------------------
 
 // MarshalArrayTransformer is a transformer (MarshalObjectTransformerFn) that transforms any array
 // or slice to a []any.
@@ -26,3 +28,20 @@ func MarshalArrayTransformer(obj any) (any, error) {
 }
 
 var _ MarshalObjectTransformerFn = MarshalArrayTransformer
+
+// MarshalMapTransformer ---------------------------------------------------------------------------
+
+// MarshalMapTransformer is a transformer (MarshalObjectTransformerFn) that transforms any map to a
+// map[any]any.
+func MarshalMapTransformer(obj any) (any, error) {
+	if kind := reflect.TypeOf(obj).Kind(); kind != reflect.Map {
+		return obj, nil
+	}
+
+	v := reflect.ValueOf(obj)
+	rv := map[any]any{}
+	for iter := v.MapRange(); iter.Next(); {
+		rv[iter.Key().Interface()] = iter.Value().Interface()
+	}
+	return rv, nil
+}
--- transformers_test.go
+++ transformers_test.go
@@ -1,7 +1,7 @@
 // Copyright 2024 Viet-Trung Luu.
 // Use of this source code is governed by the license in the LICENSE file.
 
-// This file tests arrayxform.go.
+// This file tests transformers.go.
 
 package umsgpack_test
 
@@ -12,6 +12,8 @@ import (
 
 	. "github.com/viettrungluu/umsgpack"
 )
+
+// MarshalArrayTransformer -------------------------------------------------------------------------
 
 func testMarshalArrayTransformer(t *testing.T, name string, obj any, expectedErr error, expectedOut any) {
 	if actualOut, actualErr := MarshalArrayTransformer(obj); actualErr != expectedErr {
@@ -25,6 +27,7 @@ func TestMarshalArrayTransformer_notApplicable(t *testing.T) {
 	testMarshalArrayTransformer(t, "int", int(123), nil, int(123))
 	testMarshalArrayTransformer(t, "string", "hi", nil, "hi")
 	testMarshalArrayTransformer(t, "struct", struct{}{}, nil, struct{}{})
+	testMarshalArrayTransformer(t, "map", map[string]int{"hi": 123}, nil, map[string]int{"hi": 123})
 }
 
 func TestMarshalArrayTransformer_array(t *testing.T) {
@@ -51,4 +54,37 @@ func TestMarshalArrayTransformer_MarshalToBytes(t *testing.T) {
 	}
 }
 
-// TODO: add test of MarshalArrayTransformer + Marshal.
+// MarshalMapTransformer ---------------------------------------------------------------------------
+
+func testMarshalMapTransformer(t *testing.T, name string, obj any, expectedErr error, expectedOut any) {
+	if actualOut, actualErr := MarshalMapTransformer(obj); actualErr != expectedErr {
+		t.Errorf("%v: incorrect error: actual=%v, expected=%v", name, actualErr, expectedErr)
+	} else if expectedErr == nil && !reflect.DeepEqual(actualOut, expectedOut) {
+		t.Errorf("%v: incorrect result: actual=%v, expected=%v", name, actualOut, expectedOut)
+	}
+}
+
+func TestMarshalMapTransformer_notApplicable(t *testing.T) {
+	testMarshalMapTransformer(t, "int", int(123), nil, int(123))
+	testMarshalMapTransformer(t, "struct", struct{}{}, nil, struct{}{})
+	testMarshalMapTransformer(t, "slice", []int{1, 2, 3}, nil, []int{1, 2, 3})
+}
+
+func TestMarshalMapTransformer_map(t *testing.T) {
+	testMarshalMapTransformer(t, "map[string]int", map[string]int{"hi": 123, "world": 456}, nil, map[any]any{"hi": 123, "world": 456})
+	testMarshalMapTransformer(t, "map[string]any", map[string]any{"hi": 123}, nil, map[any]any{"hi": 123})
+	testMarshalMapTransformer(t, "map[any]int", map[any]int{"hi": 123}, nil, map[any]any{"hi": 123})
+	testMarshalMapTransformer(t, "map[string]struct{}", map[any]struct{}{"hi": struct{}{}}, nil, map[any]any{"hi": struct{}{}})
+}
+
+func TestMarshalMapTransformer_MarshalToBytes(t *testing.T) {
+	opts := &MarshalOptions{
+		ApplicationMarshalObjectTransformers: []MarshalObjectTransformerFn{
+			MarshalMapTransformer,
+		},
+	}
+
+	if encoded, err := MarshalToBytes(opts, map[string]int{"0": 0}); err != nil || bytes.Compare(encoded, append([]byte{0x81}, genMapData(1)...)) != 0 {
+		t.Errorf("Unexpected result from MarshalToBytes: %v, %v", encoded, err)
+	}
+}
